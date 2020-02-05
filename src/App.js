6a2449b691bc8ebdd3775251409d0bce0cdaa1e9
jon martirosyan <jonmartirosyan1988@gmail.com>
2020-02-05 20:17:37 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App () {
+	return (
+		<div className="App">
+			<h1>Hello from Tech store</h1>
+		</div>
+	);
+}
+
+export default App;
