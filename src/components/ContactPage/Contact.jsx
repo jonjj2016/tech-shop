be0d5a2eeef177441e6edaa2eee77e35667bd1d5
jon martirosyan <jonmartirosyan1988@gmail.com>
2020-02-07 00:21:29 +0800

    implemented contact feature with the formspree.io

--- src/components/ContactPage/Contact.jsx
+++ src/components/ContactPage/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Title from '../Title';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 export default function Contact () {
 	return (
 		<section className="py-5">
 			<div className="row">
 				<div className="col-10 mx-auto col-md-6 my-3">
-					<form className="mt-5">
+					<Title title="Contact us" />
+					<form action="https://formspree.io/mrgqlqzp" method="POST" className="mt-5">
 						{/* Name */}
 						<div className="form-group">
 							<input type="text" placeholder="First Name" name="firstName" className="form-control" />
@@ -15,15 +17,23 @@ export default function Contact () {
 						<div className="form-group">
 							<input type="email" placeholder="Email" name="email" className="form-control" />
 						</div>
+						{/* Subject */}
 						<div className="form-group">
 							<input type="text" placeholder="Subject" name="subject" className="form-control" />
+						</div>
+						{/* message */}
+						<div className="form-group">
+							<textarea placeholder="Message" name="message" rows="10" className="form-control" />
+						</div>
+						<div className="form-group mt-3">
+							<input type="submit" value="Submit" className="form-control bg-primary text-white" />
 						</div>
 					</form>
 				</div>
 			</div>
 		</section>
 		// <div style={{margin}} className="contact">
-		// 	<Title center title="Contact us" />
+		//
 		// </div>
 	);
 }
